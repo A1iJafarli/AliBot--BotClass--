2d07f6abda893f7b2f5a058f7c43749fd834303d
A1iJafarli <aliaysu66@gmail.com>
2025-07-26 22:10:49 +0400

    Update botfunction.py

--- botfunction.py
+++ botfunction.py
@@ -1,21 +1,5 @@
 import random
 
-
-def sifre_olusturucu(sifre_uzunlugu):
-    ogeler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-/*!&$#?=@"
-    sifre = ""
-
-    for i in range(sifre_uzunlugu):
-        sifre += random.choice(ogeler)
-
-    return sifre
-
-
-def emoji_olusturucu():
-    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
-    return random.choice(emoji)
-
-
 def yazi_tura():
     para = random.randint(0, 2)
     if para == 0:
